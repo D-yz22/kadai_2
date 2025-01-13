19ad9332b31a60c62975b75425d88e26cfb0902d
yz-D22 <yuzunokabasawa@gmail.com>
2025-01-13 21:54:02 +0900

    Add files via upload
    2025.1.13
    9-a

---
+++ 課題2-1/scripts/script.js
@@ -0,0 +1,17 @@
+function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "ボタンが押されました!";
+    document.body.appendChild(para);
+  }
+  
+//   const buttons = document.querySelectorAll("button");
+  
+//   for (const button of buttons) {
+//     button.addEventListener("click", createParagraph);
+//   }
+  
+  const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
